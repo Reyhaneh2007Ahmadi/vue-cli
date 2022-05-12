1a6c430e6bab8f5e5a63ddfe90a18ed8a00b314f
Reyhaneh2007Ahmadi <reyhaneh.ahmadi2007@gmail.com>
2022-05-12 19:31:38 +0430

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div>task tracker</div>
+  </header>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 20px;
+}
+</style>
